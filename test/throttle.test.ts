29bab8f1fddb409f4560bac3cf90169209332409
ShroXd <ShroXdF@gmail.com>
2020-10-20 16:03:53 +0800

    test: add throttle test

--- test/throttle.test.ts
+++ test/throttle.test.ts
@@ -1,22 +1,67 @@
 // @ts-ignore
 import { throttle } from "../src";
 
+beforeEach(() => {
+  jest.useFakeTimers("modern")
+});
 
-function execManyTimes(each, times = 20) {
-  let i = 0, intervalId
+test("throttle delay", () => {
+  let fn = jest.fn()
+  let throttleFn = throttle(3000, fn)
 
-  const start = () => {
-    intervalId = setInterval(() => {
-      each();
-      if (++i === times) {
-        clearInterval(intervalId);
-      }
-    }, 20)
-  }
+  setInterval(throttleFn, 100)
+  jest.advanceTimersByTime(10000)
+  expect(fn).toHaveBeenCalledTimes(3)
+})
 
-  start()
-}
+test("throttle leading", () => {
+  let fn = jest.fn()
+  let throttleFn = throttle({
+    delay: 3000,
+    leading: true
+  }, fn)
 
-test("throttle", () => {
-  expect(1).toBe(1)
+  setInterval(throttleFn, 100)
+  jest.advanceTimersByTime(10000)
+  expect(fn).toHaveBeenCalledTimes(4)
+})
+
+test("throttle trailing", () => {
+  let fn = jest.fn()
+  let throttleFn = throttle({
+    delay: 3000,
+    leading: false,
+    trailing: true
+  }, fn)
+
+  const timer = setInterval(throttleFn, 100)
+  jest.advanceTimersByTime(12000)
+  clearInterval(timer)
+  jest.advanceTimersByTime(12000)
+  expect(fn).toHaveBeenCalledTimes(4)
+})
+
+test("throttle leading trailing", () => {
+  let fn = jest.fn()
+  let throttleFn = throttle({
+    delay: 3000,
+    leading: true,
+    trailing: true
+  }, fn)
+
+  const timer = setInterval(throttleFn, 100)
+  jest.advanceTimersByTime(12000)
+  clearInterval(timer)
+  jest.advanceTimersByTime(12000)
+  expect(fn).toHaveBeenCalledTimes(5)
+})
+
+test("throttle cancel", () => {
+  let fn = jest.fn()
+  let throttleFn = throttle(3000, fn)
+
+  const timer = setInterval(throttleFn, 100)
+  throttleFn.cancel()
+  jest.advanceTimersByTime(12000)
+  expect(fn).not.toBeCalled()
 })
